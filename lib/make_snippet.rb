d1f8548f69428f952142e680b42dccef3e57ae16
sunaina rishi <sunaina.d.rishi@gmail.com>
2023-03-15 13:09:11 +0000

    Refactored make_snippet from video

--- lib/make_snippet.rb
+++ lib/make_snippet.rb
@@ -3,7 +3,8 @@
 def make_snippet(str)
   string_array = str.split()
  if string_array.length > 5
-    string_array[0..4].join(" ") + "..."
+    first_five_words = string_array[0..4].join(" ")
+    first_five_words + "..."
   else
     str
   end
