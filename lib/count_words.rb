bb49f4e3d7db53d86746eee2ce73c4fc6cefb44c
sunaina rishi <sunaina.d.rishi@gmail.com>
2023-03-16 15:42:53 +0000

    Added a test for Runtime Error

--- lib/count_words.rb
+++ lib/count_words.rb
@@ -1,12 +1,16 @@
 def count_words(str)
-  string_array = str.split
-  new_array =[]
-  string_array.each do |word|
-    if word =~ /[\w]/
-       new_array.push(word)
-    end
+  if str.is_a? String
+    string_array = str.split
+    new_array =[]
+    string_array.each do |word|
+      if word =~ /[\w]/
+        new_array.push(word)
+      end
   end
-  new_array.length
+    new_array.length
+  else
+    raise RuntimeError
+  end
 end
 
 
--- spec/count_words_spec.rb
+++ spec/count_words_spec.rb
@@ -1,6 +1,11 @@
 require 'count_words'
 
 RSpec.describe "count_words method" do
+  it "takes an empty string and returns 0" do
+    result = count_words("")
+    expect(result).to eq 0
+  end
+
   it "Returns number of words for string" do
     result = count_words("The dog is called Sirius")
     expect(result).to eq 5
@@ -19,5 +24,9 @@ RSpec.describe "count_words method" do
   it "Returns number of words for a string with numbers and double punctuation" do
     result = count_words("The dog is 3 called -- Sirius!")
     expect(result).to eq 6
+  end
+
+  it "returns an error message for a non-string" do
+    expect { count_words(123) }.to raise_error RuntimeError
   end
 end
