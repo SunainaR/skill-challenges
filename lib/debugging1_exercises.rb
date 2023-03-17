67e3071afc291a02753b48e54f7edfdc371c93dd
sunaina rishi <sunaina.d.rishi@gmail.com>
2023-03-17 02:11:32 +0000

    Debugging Exercise completed

---
+++ lib/debugging1_exercises.rb
@@ -0,0 +1,72 @@
+# Exercise 1
+
+=begin
+# Code with bugs: 
+
+def say_hello(name)
+  return "hello #(name)"
+end
+
+=end
+
+# Intended output:
+#
+# > say_hello("kay")
+# => "hello kay"
+
+# Debugged code:
+
+def say_hello(name)
+  return "hello #{name}"
+end
+
+# ---------------------------------
+
+# Exercise 2
+
+=begin
+# Code with bugs:
+
+def encode(plaintext, key)
+  cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+  ciphertext_chars = plaintext.chars.map do |char|
+    (65 + cipher.find_index(char)).chr
+  end
+  return ciphertext_chars.join
+end
+
+def decode(ciphertext, key)
+  cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+  plaintext_chars = ciphertext.chars.map do |char|
+    cipher[65 - char.ord]
+  end
+  return plaintext_chars.join
+end
+
+=end
+
+# Debugged code
+
+def encode(plaintext, key)
+  cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
+  ciphertext_chars = plaintext.chars.map do |char|
+    (65 + cipher.find_index(char)).chr
+  end
+   puts ciphertext_chars.join
+end
+
+def decode(ciphertext, key)
+  cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
+  plaintext_chars = ciphertext.chars.map do |char|
+    cipher[char.ord - 65]
+  end
+  puts plaintext_chars.join
+end
+
+# Intended output:
+#
+# > encode("theswiftfoxjumpedoverthelazydog", "secretkey")
+# => "EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL"
+#
+# > decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
+# => "theswiftfoxjumpedoverthelazydog"
