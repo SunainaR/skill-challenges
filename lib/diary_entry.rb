42b5adab77fe9e4645d6dcf549c2075b59f3918d
sunaina rishi <sunaina.d.rishi@gmail.com>
2023-03-18 00:04:21 +0000

    Step 5: Exercise Diary Entry - further steps added

--- lib/diary_entry.rb
+++ lib/diary_entry.rb
@@ -1,7 +1,8 @@
 class DiaryEntry
-  def initialize(title, contents) # title, contents are strings
+  def initialize(title, contents) # title, contents are @contentsings
     @title = title
     @contents = contents
+    @word_count
   end
 
   def title
@@ -13,19 +14,32 @@ class DiaryEntry
   end
 
   def count_words
-    # Returns the number of words in the contents as an integer
+      @word_count = @contents.split.length
   end
 
-  def reading_time(wpm) # wpm is an integer representing the number of words the
-                        # user can read per minute
-    # Returns an integer representing an estimate of the reading time in minutes
-    # for the contents at the given wpm.
+  def reading_time(wpm) 
+    # full_read_time = (@word_count * 60)/wpm.to_f.round
+    full_read_time_mins = (@word_count/wpm.to_f).round
   end
 
-  def reading_chunk(wpm, minutes) # `wpm` is an integer representing the number
-                                  # of words the user can read per minute
-                                  # `minutes` is an integer representing the
-                                  # number of minutes the user has to read
+  def reading_chunk(wpm, minutes) 
+
+    (self.reading_time(wpm)/minutes.to_f).round
+
+    # word_position = 0
+=begin
+    while word_position < self.count_words
+     
+     
+
+    end
+=end
+
+    
+    # `wpm` is an integer representing the number
+    # of words the user can read per minute
+    # `minutes` is an integer representing the
+    # number of minutes the user has to read
     # Returns a string with a chunk of the contents that the user could read
     # in the given number of minutes.
     # If called again, `reading_chunk` should return the next chunk, skipping
@@ -33,3 +47,12 @@ class DiaryEntry
     # The next call after that it should restart from the beginning.
   end
 end
+
+
+
+
+#entry = DiaryEntry.new("First Day", "one " * 500)
+entry = DiaryEntry.new("First Day", (("one " * 400) + ("two " * 200) + ("three " * 400 )+ ("four " * 1000)))
+p entry.count_words
+p entry.reading_time(200)
+p entry.reading_chunk(100, 2) 
--- spec/diary_entry_spec.rb
+++ spec/diary_entry_spec.rb
@@ -1,16 +1,44 @@
 require 'diary_entry'
 
 RSpec.describe DiaryEntry do
-  context "For two methods to return variables" do
+  context "For two methods to return data in title and contents variables" do
     it "returns the title of a DiaryEntry" do
       entry = DiaryEntry.new("First Day", "I read a book.")
       result = entry.title
       expect(result).to eq "First Day"
     end
+
     it "returns the contents of a DiaryEntry" do
       entry = DiaryEntry.new("First Day", "I read a book.")
       result = entry.contents
       expect(result).to eq "I read a book."
+    end
+  end
+
+  context "For count_words method" do
+    it "returns the word_count of a DiaryEntry" do
+      entry = DiaryEntry.new("First Day", "I read a book.")
+      result = entry.count_words
+      expect(result).to eq 4
+    end
+  end
+
+  context "For reading_time method" do
+    it "returns the reading_time for the entire contents" do
+      entry = DiaryEntry.new("First Day", "one " * 2000)
+      entry.count_words
+      result = entry.reading_time(200)
+      expect(result).to eq 10
+    end
+  end
+
+  context "For reading_chunk method" do
+    xit "returns " do
+      entry = DiaryEntry.new("First Day", (("one " * 400) + ("two " * 200) + ("three " * 400 )+ ("four " * 1000)))
+      entry.count_words
+      entry.reading_time(200)
+      result = entry.reading_chunk(100, 5)
+      expect(result).to eq (("one " * 400) + ("two " * 100))
     end
   end
 end
