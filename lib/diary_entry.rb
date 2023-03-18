2643d07a4a55bb963434c94ce0066c64192c8093
sunaina rishi <sunaina.d.rishi@gmail.com>
2023-03-18 01:42:08 +0000

    Step 5: Exercise on diary entry - refactored initialized instance variable

--- lib/diary_entry.rb
+++ lib/diary_entry.rb
@@ -1,41 +1,40 @@
 class DiaryEntry
+  # The are all initialised when the object of this class is first created. 
+  # So when methods are called, they could change the state of these instance variables.
   def initialize(title, contents) # title, contents are @contentsings
     @title = title
     @contents = contents
     @word_count
+    @previously_read_index = 0 
   end
 
   def title
+  # Returns the title as a string
     @title
   end
 
   def contents
+# Returns the contents as a string
     @contents
   end
 
   def count_words
+# Returns the number of words in the contents as an integer
       @word_count = @contents.split.length
   end
 
   def reading_time(wpm) 
+# wpm is an integer representing the number of words the
+# user can read per minute
+# Returns an integer representing an estimate of the reading time in minutes
+# for the contents at the given wpm.
+
     # full_read_time = (@word_count * 60)/wpm.to_f.round
     full_read_time_mins = (@word_count/wpm.to_f).round
   end
 
   def reading_chunk(wpm, minutes) 
 
-    (self.reading_time(wpm)/minutes.to_f).round
-
-    # word_position = 0
-=begin
-    while word_position < self.count_words
-     
-     
-
-    end
-=end
-
-    
     # `wpm` is an integer representing the number
     # of words the user can read per minute
     # `minutes` is an integer representing the
@@ -44,15 +43,29 @@ class DiaryEntry
     # in the given number of minutes.
     # If called again, `reading_chunk` should return the next chunk, skipping
     # what has already been read, until the contents is fully read.
-    # The next call after that it should restart from the beginning.
+    # The next call after that it should restart from the beginning
+
+    # @previously_read_index.nil? ? @previously_read_index = 0 : @previously_read_index
+    readable_length = wpm * minutes
+    next_read_index = @previously_read_index + readable_length
+    chunk_to_read = @contents.split[@previously_read_index...next_read_index].join(" ").strip
+    next_read_index >= self.count_words ? @previously_read_index = 0 : @previously_read_index = next_read_index
+    return chunk_to_read
   end
 end
 
 
 
+=begin
 
 #entry = DiaryEntry.new("First Day", "one " * 500)
 entry = DiaryEntry.new("First Day", (("one " * 400) + ("two " * 200) + ("three " * 400 )+ ("four " * 1000)))
 p entry.count_words
-p entry.reading_time(200)
-p entry.reading_chunk(100, 2) 
+p entry.reading_time(100)
+p entry.reading_chunk(100, 5) 
+
+entry = DiaryEntry.new("First Day", (("one " * 400) + ("two " * 200) + ("three " * 400 )+ ("four " * 1000)))
+entry.count_words
+entry.reading_chunk(100, 5)
+p entry.reading_chunk(200, 2)
+=end
--- spec/diary_entry_spec.rb
+++ spec/diary_entry_spec.rb
@@ -33,12 +33,36 @@ RSpec.describe DiaryEntry do
   end
 
   context "For reading_chunk method" do
-    xit "returns " do
+    it "returns " do
       entry = DiaryEntry.new("First Day", (("one " * 400) + ("two " * 200) + ("three " * 400 )+ ("four " * 1000)))
       entry.count_words
       entry.reading_time(200)
       result = entry.reading_chunk(100, 5)
-      expect(result).to eq (("one " * 400) + ("two " * 100))
+      expect(result).to eq (("one " * 400) + ("two " * 100)).strip
+    end
+    it "returns a chunk to read after calling reading chunk twice before end of contents" do
+      entry = DiaryEntry.new("First Day", (("one " * 400) + ("two " * 200) + ("three " * 400 )+ ("four " * 1000)))
+      entry.count_words
+      entry.reading_chunk(100, 5)
+      result = entry.reading_chunk(200, 2)
+      expect(result).to eq (("two " * 100)+("three " * 300)).strip
+    end
+    it "returns a chunk to read just until end of contents even though have more time to read" do
+      entry = DiaryEntry.new("First Day", (("one " * 400) + ("two " * 200) + ("three " * 400 )+ ("four " * 1000)))
+      entry.count_words
+      entry.reading_chunk(100, 4)
+      entry.reading_chunk(100, 15)
+      result = entry.reading_chunk(200, 1)
+      expect(result).to eq ("four " * 100).strip
+    end
+    it "returns a chunk to read from beginning as reached end of text previously" do
+      entry = DiaryEntry.new("First Day", (("one " * 400) + ("two " * 200) + ("three " * 400 )+ ("four " * 1000)))
+      entry.count_words
+      entry.reading_chunk(100, 4)
+      entry.reading_chunk(100, 15)
+      entry.reading_chunk(200, 1)
+      result = entry.reading_chunk(200, 3)
+      expect(result).to eq (("one " * 400) + ("two " * 200)).strip
     end
   end
 end
